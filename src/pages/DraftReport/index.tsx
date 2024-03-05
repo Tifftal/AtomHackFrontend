2d2757dd922b7ac53ac9e6d7988f3bd0cf84ecc8
VarTy <talankina.v@mail.ru>
2024-03-05 11:53:21 +0300

    file input

--- src/pages/DraftReport/index.tsx
+++ src/pages/DraftReport/index.tsx
@@ -1,14 +1,37 @@
 import TextEditor from '../../widgets/TextEditor'
+import { ActionIcon, Button } from '@mantine/core';
+import { useState } from 'react';
+import { FileButton, Group } from '@mantine/core';
+import { IconPaperclip } from '@tabler/icons-react';
 import './index.scss'
-import { Button } from '@mantine/core';
 
 const DraftReport = () => {
+    const [files, setFiles] = useState<File[]>([]);
+
     return (
         <div className='draft-report'>
             <TextEditor />
-            <div className='group-btn'>
-                <Button variant="filled" color="gray">Сохранить черновик</Button>
-                <Button variant="filled" color="cyan">Отправить</Button>
+
+            {files.length > 0 && (
+                <div className='file'>
+                    {files.map((file, index) => (
+                        <p key={index}>{file.name}</p>
+                    ))}
+                </div>
+            )}
+
+            <div className='footer-report'>
+                <div className='group-btn'>
+                    <Button variant="filled" color="cyan">Отправить</Button>
+                    <Button variant="filled" color="gray">Сохранить черновик</Button>
+                    <Group justify="center">
+                        <FileButton onChange={setFiles} accept="image/png,image/jpeg" multiple>
+                            {(props) => <ActionIcon variant="white" color="rgba(0, 0, 0, 1)" size="lg" aria-label="Settings" {...props}>
+                                <IconPaperclip style={{ width: '80%', height: '80%' }} stroke={1.5} />
+                            </ActionIcon>}
+                        </FileButton>
+                    </Group>
+                </div>
             </div>
         </div>
     )
