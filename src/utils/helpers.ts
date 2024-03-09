907fbec542c8f950ee81a8beae7b00efc2456ac8
VarTy <talankina.v@mail.ru>
2024-03-09 21:36:20 +0300

    fixation of ReportDetailed's layout

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,19 +1,3 @@
-export const formatTime = (time: Date | undefined) => {
-    if (!time) {
-        return 'Undefined';
-    }
-
-    let hours = String(time.getHours()).padStart(0, '2');
-    if (Number(hours) < 10) {
-        hours = `0${hours}`
-    }
-    let minutes = String(time.getHours()).padStart(0, '2');
-    if (Number(minutes) < 10) {
-        minutes = `0${minutes}`
-    }
-    return `${hours}:${minutes}`;
-};
-
 export const TruncateText = (text: string, maxLength: number) => {
     if (text.length > maxLength) {
         return `${text.substring(0, maxLength / 2)}...${text.substring(text.length - maxLength / 2)}`;
